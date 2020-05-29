248ba93ca3c1c8f2f2d2949dd12bbe39aef3ed24
Terry Lin <terry@fontrip.com>
2020-05-29 17:43:48 +0800

    Stream ok.

--- src/Psr7/Stream.php
+++ src/Psr7/Stream.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 namespace Shieldon\Psr7;
 
 use Psr\Http\Message\StreamInterface;
+use InvalidArgumentException;
+use RuntimeException;
 
 /*
  * Describes a data stream.
@@ -20,17 +22,17 @@ use Psr\Http\Message\StreamInterface;
 class Stream implements StreamInterface
 {
     /**
-     * @var bool|null
+     * @var bool
      */
     protected $readable;
 
     /**
-     * @var bool|null
+     * @var bool
      */
     protected $writable;
 
     /**
-     * @var bool|null
+     * @var bool
      */
     protected $seekable;
 
@@ -42,200 +44,335 @@ class Stream implements StreamInterface
     protected $size;
 
     /**
-     * Stream constructor
-     */
-    public function __construct($stream, $metadata = [])
-    {
-        
-    }
-
-    /**
-     * Reads all data from the stream into a string, from the beginning to end.
-     *
-     * This method MUST attempt to seek to the beginning of the stream before
-     * reading data and read the stream until the end is reached.
-     *
-     * Warning: This could attempt to load a large amount of data into memory.
-     *
-     * This method MUST NOT raise an exception in order to conform with PHP's
-     * string casting operations.
-     *
-     * @see http://php.net/manual/en/language.oop5.magic.php#object.tostring
-     * @return string
-     */
-    public function __toString()
-    {
-
-    }
-
-    /**
-     * Closes the stream and any underlying resources.
-     *
-     * @return void
-     */
-    public function close()
-    {
-
-    }
-
-    /**
-     * Separates any underlying resources from the stream.
-     *
-     * After the stream has been detached, the stream is in an unusable state.
-     *
-     * @return resource|null Underlying PHP stream, if any
-     */
-    public function detach()
-    {
-
-    }
-
-    /**
-     * Get the size of the stream if known.
-     *
-     * @return int|null Returns the size in bytes if known, or null if unknown.
-     */
-    public function getSize()
-    {
-
-    }
-
-    /**
-     * Returns the current position of the file read/write pointer
-     *
-     * @return int Position of the file pointer
-     * @throws \RuntimeException on error.
-     */
-    public function tell()
-    {
-
-    }
-
-    /**
-     * Returns true if the stream is at the end of the stream.
-     *
-     * @return bool
-     */
-    public function eof()
-    {
-
-    }
-
-    /**
-     * Returns whether or not the stream is seekable.
-     *
-     * @return bool
-     */
-    public function isSeekable()
-    {
-
-    }
-
-    /**
-     * Seek to a position in the stream.
-     *
-     * @see http://www.php.net/manual/en/function.fseek.php
-     * @param int $offset Stream offset
-     * @param int $whence Specifies how the cursor position will be calculated
-     *     based on the seek offset. Valid values are identical to the built-in
-     *     PHP $whence values for `fseek()`.  SEEK_SET: Set position equal to
-     *     offset bytes SEEK_CUR: Set position to current location plus offset
-     *     SEEK_END: Set position to end-of-stream plus offset.
-     * @throws \RuntimeException on failure.
-     */
-    public function seek($offset, $whence = SEEK_SET)
-    {
-
-    }
-
-    /**
-     * Seek to the beginning of the stream.
-     *
-     * If the stream is not seekable, this method will raise an exception;
-     * otherwise, it will perform a seek(0).
-     *
-     * @see seek()
-     * @see http://www.php.net/manual/en/function.fseek.php
-     * @throws \RuntimeException on failure.
-     */
-    public function rewind()
-    {
-
-    }
-
-    /**
-     * Returns whether or not the stream is writable.
-     *
-     * @return bool
-     */
-    public function isWritable()
-    {
-
-    }
-
-    /**
-     * Write data to the stream.
-     *
-     * @param string $string The string that is to be written.
-     * @return int Returns the number of bytes written to the stream.
-     * @throws \RuntimeException on failure.
-     */
-    public function write($string)
-    {
-
-    }
-
-    /**
-     * Returns whether or not the stream is readable.
-     *
-     * @return bool
-     */
-    public function isReadable()
-    {
-
-    }
-
-    /**
-     * Read data from the stream.
-     *
-     * @param int $length Read up to $length bytes from the object and return
-     *     them. Fewer than $length bytes may be returned if underlying stream
-     *     call returns fewer bytes.
-     * @return string Returns the data read from the stream, or an empty string
-     *     if no bytes are available.
-     * @throws \RuntimeException if an error occurs.
-     */
-    public function read($length)
-    {
-
-    }
-
-    /**
-     * Returns the remaining contents in a string
-     *
-     * @return string
-     * @throws \RuntimeException if unable to read.
-     * @throws \RuntimeException if error occurs while reading.
-     */
-    public function getContents()
-    {
-
-    }
-
-    /**
-     * Get stream metadata as an associative array or retrieve a specific key.
-     *
      * The keys returned are identical to the keys returned from PHP's
      * stream_get_meta_data() function.
      *
-     * @see http://php.net/manual/en/function.stream-get-meta-data.php
-     * @param string $key Specific metadata to retrieve.
-     * @return array|mixed|null Returns an associative array if no key is
-     *     provided. Returns a specific key value if a key is provided and the
-     *     value is found, or null if the key is not found.
+     * @var array
+     */
+    protected $meta;
+
+    /**
+     * Typically a PHP resource.
+     *
+     * @var resource
+     */
+    protected $stream;
+
+    /**
+     * Stream constructor
+     * 
+     * @param resource $stream A valid resource.
+     */
+    public function __construct($stream)
+    {
+        $this->assertStream($stream);
+        $this->stream = $stream;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isWritable(): bool
+    {
+        return $this->writable;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isReadable(): bool
+    {
+        return $this->readable;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isSeekable(): bool
+    {
+        return $this->seekable;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function close(): void
+    {
+        if (isset($this->stream)) {
+            $this->detach();
+        }
+
+        if (is_resource($this->stream)) {
+            fclose($this->stream);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function detach()
+    {
+        if (! isset($this->stream) || ! $this->stream) {
+            return null;
+        }
+
+        $legacy = $this->stream;
+        
+        $this->readable = false;
+        $this->writable = false;
+        $this->seekable = false;
+        $this->size = null;
+        $this->meta = [];
+
+        unset($this->stream);
+
+        return $legacy;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSize()
+    {
+        if (! isset($this->stream) || ! $this->stream) {
+            return null;
+        }
+
+        if (! $this->size) {
+            $stats = fstat($this->stream);
+
+            if ($stats) {
+                $this->size = isset($stats['size']) ? $stats['size'] : null;
+            }
+        }
+
+        return $this->size;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function tell(): int
+    {
+        if (! isset($this->stream) || ! $this->stream) {
+            throw new RuntimeException(
+                'Stream does not exist.'
+            );
+        }
+
+        $pointer = false;
+
+        if ($this->stream) {
+            $pointer = ftell($this->stream);
+        }
+
+        if ($pointer === false) {
+            throw new RuntimeException(
+                'Unable to get the position of the file pointer in stream.'
+            );
+        }
+
+        return $pointer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function eof()
+    {
+        return $this->stream ? feof($this->stream) : true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function seek($offset, $whence = SEEK_SET): void
+    {
+        if (! isset($this->stream) || ! $this->stream) {
+            throw new RuntimeException(
+                'Stream does not exist.'
+            );
+        }
+
+        if (! $this->seekable()) {
+            throw new RuntimeException(
+                'Stream is not seekable.'
+            );
+        }
+
+        $offset = (int) $offset;
+        $whence = (int) $whence;
+
+        switch ($whence) {
+
+            case SEEK_SET:
+                $message = 'Set position equal to offset bytes.';
+                break;
+
+            case SEEK_CUR:
+                $message = 'Set position to current location plus offset.';
+                break;
+    
+            case SEEK_END:
+                $message = 'Set position to end-of-stream plus offset.';
+                break;
+
+            default:
+                $message = 'Unknown error.';
+                break;
+        }
+
+        if (fseek($this->stream, $offset, $whence) === -1) {
+            throw new RuntimeException(
+                sprintf(
+                    '%s. Unable to seek to stream at position %s',
+                    $message,
+                    $offset
+                )
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rewind()
+    {
+        $this->seek(0);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($string)
+    {
+        if (! isset($this->stream) || ! $this->stream) {
+            throw new RuntimeException(
+                'Stream does not exist.'
+            );
+        }
+
+        $size = 0;
+
+        if ($this->isWritable()) {
+            $size = fwrite($this->stream, $string);
+        }
+
+        if ($size === false) {
+            throw new RuntimeException(
+                'Unable to write to stream.'
+            );
+        }
+
+        // Make sure that `getSize()`will count the correct size again after writting anything.
+        $this->size = null;
+
+        return $size;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($length)
+    {
+        if (! isset($this->stream) || ! $this->stream) {
+            throw new RuntimeException(
+                'Stream does not exist.'
+            );
+        }
+
+        $string = false;
+
+        if ($this->isReadable()) {
+            $string = fread($this->stream, $length);
+        }
+
+        if ($string === false) {
+            throw new RuntimeException(
+                'Unable to read from stream.'
+            );
+        }
+
+        return $string;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getContents()
+    {
+        if (! isset($this->stream) || ! $this->stream) {
+            throw new RuntimeException(
+                'Stream does not exist.'
+            );
+        }
+
+        $string = false;
+
+        if ($this->isReadable()) {
+            $string = stream_get_contents($this->stream);
+        }
+
+        if ($string === false) {
+            throw new RuntimeException(
+                'Unable to read stream contents.'
+            );
+        }
+
+        return $string;
+    }
+
+    /**
+     * {@inheritdoc}
      */
     public function getMetadata($key = null)
     {
+        if ($this->stream) {
+            $this->meta = stream_get_meta_data($this->stream);
+            
+            if (! $key) {
+                return $this->meta;
+            }
 
+            if (isset($this->meta[$key])) {
+                return $this->meta[$key];
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString()
+    {
+        if ($this->isSeekable()) {
+            $this->rewind();
+        }
+
+        return $this->getContents();
+    }
+
+
+    /**
+     * Throw exception if stream is not a valid PHP resource.
+     *
+     * @param resource $stream A valid resource.
+     * 
+     * @return void
+     * 
+     * InvalidArgumentException
+     */
+    protected function assertStream($stream): void
+    {
+        if (! is_resource($stream)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Stream should be a resource, but %s provided.',
+                    gettype(stream)
+                )
+            );
+        }
     }
 }
