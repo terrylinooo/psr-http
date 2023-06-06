082853b0fc47e6878e5df4eae46fd9ad412d8d38
Terry Lin <terrylin.developer@gmail.com>
2023-06-06 15:56:35 +0800

    Add a return type hint compatible with the psr/http-message package.

--- src/Psr17/StreamFactory.php
+++ src/Psr17/StreamFactory.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * This file is part of the Shieldon package.
  *
  * (c) Terry L. <contact@terryl.in>
@@ -120,8 +120,4 @@ class StreamFactory implements StreamFactoryInterface
             );
         }
     }
-
-
-
-
 }
--- src/Psr17/UploadedFileFactory.php
+++ src/Psr17/UploadedFileFactory.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * This file is part of the Shieldon package.
  *
  * (c) Terry L. <contact@terryl.in>
